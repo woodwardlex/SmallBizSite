366c02a6b307bea61b0e2b9cb5e8d6e7f2a6e026
Alexis Woodward <alexis.woodward1717@outlook.com>
2020-11-12 12:24:54 -0600

    IdentitySample - completed steps for Identity framework

--- SmallBizSite.UI.MVC/Startup.cs
+++ SmallBizSite.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(SmallBizSite.UI.MVC.Startup))]
 namespace SmallBizSite.UI.MVC
 {
     public partial class Startup
